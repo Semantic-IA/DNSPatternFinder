3cb2a245947b40d5aa1a33b96ae4afb30e56a2d8
Max Maass <0maass@informatik.uni-hamburg.de>
2013-03-13 14:03:02 +0000

    Derp

--- src/DNSPatternFinder.py
+++ src/DNSPatternFinder.py
@@ -7,11 +7,11 @@ import IO.OutputWriter
 import IO.CSVParser
 import Worker.WorkerThread
 
-parser = IO.CSVParser.Parser("/home/max/Downloads/top-1m-ns-nc-2.csv").getJob()
-writer = IO.OutputWriter.writer("/home/max/outfile.txt")
+parser = IO.CSVParser.Parser("/home/Max/Downloads/top-1m-ns-nc-2.csv").getJob()
+writer = IO.OutputWriter.writer("/home/Max/outfile.txt")
 threads = []
 for i in range(4):
-    t = Worker.WorkerThread.Thread("/home/max/Downloads/phantomjs-1.8.2-linux-x86_64/bin/phantomjs", "/home/max/Downloads/phantomjs-1.8.2-linux-x86_64/netdomains.js",parser,writer)
+    t = Worker.WorkerThread.Thread("/home/Max/Downloads/phantomjs-1.8.2-linux-x86_64/bin/phantomjs", "/home/Max/Downloads/phantomjs-1.8.2-linux-x86_64/netdomains.js",parser,writer)
     threads.append(t)
 [x.start() for x in threads]
 [x.join() for x in threads]
